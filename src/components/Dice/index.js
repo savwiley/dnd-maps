85eadc1d64bb2727c005a832fc2cc071ec082132
Savannaha <savwiley@yahoo.com>
2022-09-24 18:12:53 -0500

    create form for dice roller

--- src/components/Dice/index.js
+++ src/components/Dice/index.js
@@ -1,9 +1,27 @@
-import React from "react";
+import React, { useState } from "react";
+import { DiceBox, Answer } from "./style"
 
 const Dice = () => {
+  const [amount, setAmount] = useState(1);
+  const [sides, setSides] = useState(20);
+  const [modifier, setModifier] = useState(0);
+
+
+
   return (
-    <>
-    </>
+    <DiceBox>
+
+      <input type="text" id="amount" value={amount} /> 
+      d 
+      <input type="text" id="sides" value={sides} /> 
+      + 
+      <input type="text" id="modifier" value={modifier} />
+
+      <input type="button" onClick={} />
+
+      <Answer></Answer>
+
+    </DiceBox>
   )
 };
 
--- src/components/Dice/style.js
+++ src/components/Dice/style.js
@@ -1,1 +1,11 @@
 import styled from "styled-components";
+
+export const DiceBox = styled.div`
+  position: fixed;
+  bottom: 0;
+  left: 0;
+  right: 0;
+  height: 200px;
+  padding: 20px;
+  box-shadow: 0 0 4px rgba(0, 0, 0, 0.4);
+`;
