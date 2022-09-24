af0fa068b8fa01e2925d580768129c4dccbf5b94
Savannaha <savwiley@yahoo.com>
2022-09-24 18:12:39 -0500

    stylize body

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ function App() {
   return (
     <div>
       <Grid />
+      <Dice />
     </div>
   );
 }
--- src/components/Grid/index.js
+++ src/components/Grid/index.js
@@ -1,10 +1,7 @@
 import React from "react";
 
 const Grid = () => {
-  return (
-    <>
-    </>
-  )
+  return <></>;
 };
 
 export default Grid;
