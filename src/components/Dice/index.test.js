a62d63b419b28b9410068c20df3b06e3a35ecf70
Savannaha <savwiley@yahoo.com>
2022-09-26 22:56:22 -0500

    formatted code

--- src/components/Dice/index.test.js
+++ src/components/Dice/index.test.js
@@ -1,6 +1,6 @@
-const rollDice = require('./rollDice');
+const rollDice = require("./rollDice");
 
-test('rolls dice', () => {
+test("rolls dice", () => {
   expect(rollDice(1, 20, 2)).toBeGreaterThan(2);
   expect(rollDice(1, 20, 2)).toBeLessThan(22);
 });
