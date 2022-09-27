8f9ad7dd302b2b15166213c3401a977881e772f4
Savannaha <savwiley@yahoo.com>
2022-09-26 22:56:47 -0500

    moved inputs to separate component

--- src/components/Dice/index.js
+++ src/components/Dice/index.js
@@ -1,46 +1,30 @@
 import React, { useState } from "react";
-import { rollDice } from "./rollDice";
-import { DiceBox } from "./style"
+import { RollDice } from "./rollDice";
+import { DiceBox } from "./style";
 
 const Dice = () => {
   const [amount, setAmount] = useState(1);
   const [sides, setSides] = useState(20);
   const [modifier, setModifier] = useState(0);
 
-
-
   return (
     <DiceBox>
-
-      <input 
-        type="text" 
-        id="amount" 
-        value={amount} 
-        onChange={(e) => setAmount(e.target.value)} 
-      /> 
-      d 
-      <input 
-        type="text" 
-        id="sides" 
-        value={sides} 
-        onChange={(e) => setSides(e.target.value)} 
-      /> 
-      + 
-      <input 
-        type="text" 
-        id="modifier" 
-        value={modifier} 
-        onChange={(e) => setModifier(e.target.value)} 
+      <RollDice
+        useAmount={setAmount}
+        useSides={setSides}
+        useModifier={setModifier}
+        amount={amount}
+        sides={sides}
+        modifier={modifier}
       />
-
-      <input 
-        type="button" 
+      
+      <input
+        type="button"
         value="Roll!"
-        onClick={rollDice(amount, sides, modifier)} 
+        onClick={rollDice(amount, sides, modifier)}
       />
-
     </DiceBox>
-  )
+  );
 };
 
 export default Dice;
