0c4d18d88b06fa144f533061088f1f7c6ab21928
Savannaha <savwiley@yahoo.com>
2022-09-26 23:24:36 -0500

    make function to roll dice

--- src/components/Dice/index.js
+++ src/components/Dice/index.js
@@ -1,11 +1,20 @@
-import React, { useState } from "react";
-import { RollDice } from "./rollDice";
+import React, { useState, useEffect } from "react";
+import { RollDice } from "./RollDice";
 import { DiceBox } from "./style";
 
 const Dice = () => {
   const [amount, setAmount] = useState(1);
   const [sides, setSides] = useState(20);
   const [modifier, setModifier] = useState(0);
+  const [change, setChange] = useState(false);
+  const [answer, setAnswer] = useState();
+
+  useEffect(() => {
+    for (let i = 0; i < amount; i++) {
+      let rolled = Math.floor(Math.random * sides) + modifier;
+      setAnswer(`1 d${sides} + ${modifier} = ${rolled}`);
+    }
+  }, [change]);
 
   return (
     <DiceBox>
@@ -17,12 +26,14 @@ const Dice = () => {
         sides={sides}
         modifier={modifier}
       />
-      
+
       <input
         type="button"
         value="Roll!"
-        onClick={rollDice(amount, sides, modifier)}
+        onClick={change ? setChange(false) : setChange(true)}
       />
+
+      <AnswerBox answer={answer} />
     </DiceBox>
   );
 };
