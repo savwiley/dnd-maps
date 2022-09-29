52491daaf63b56d2ce3cf0848c4f800981d36239
Savannaha <savwiley@yahoo.com>
2022-09-29 14:41:23 -0500

    display multiple dice at once

--- src/components/Dice/AnswerBox.js
+++ src/components/Dice/AnswerBox.js
@@ -1,14 +1,21 @@
 import React from "react";
+import { Rolled } from "./style";
 import PropTypes from "prop-types";
 
 const AnswerBox = (props) => {
   const { answer } = props;
 
-  return <>{answer}</>;
+  return (
+    <>
+      {answer.map((a, i) => (
+        <Rolled key={i}>{a}</Rolled>
+      ))}
+    </>
+  )
 };
 
 AnswerBox.propTypes = {
-  answer: PropTypes.PropTypes.oneOfType([PropTypes.object, PropTypes.string]),
+  answer: PropTypes.PropTypes.oneOfType([PropTypes.object, PropTypes.string, PropTypes.array]),
 };
 
 export default AnswerBox;
