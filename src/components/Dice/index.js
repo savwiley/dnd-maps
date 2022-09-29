da5ef61cff0866a808381923838a085f8ec30ce3
Savannaha <savwiley@yahoo.com>
2022-09-29 14:21:58 -0500

    fix dice roller

--- src/components/Dice/index.js
+++ src/components/Dice/index.js
@@ -1,38 +1,56 @@
-import React, { useState, useEffect } from "react";
-import RollDice from "./RollDice";
+import React, { useState } from "react";
 import AnswerBox from "./AnswerBox";
-import { DiceBox } from "./style";
+import { DiceBox, Inputs } from "./style";
 
 const Dice = () => {
-  const [amount, setAmount] = useState(1);
-  const [sides, setSides] = useState(20);
-  const [modifier, setModifier] = useState(0);
-  const [change, setChange] = useState(false);
-  const [answer, setAnswer] = useState();
+  const [answer, setAnswer] = useState("rolling...");
 
-  useEffect(() => {
+  let amount = 1;
+  let sides = 20;
+  let modifier = 0;
+
+  const roll = (amount, sides, modifier) => {
     for (let i = 0; i < amount; i++) {
-      let rolled = Math.floor(Math.random * sides) + modifier;
+      let rolled = Math.floor(Math.random() * sides) + 1 + modifier;
       setAnswer(`1 d${sides} + ${modifier} = ${rolled}`);
+      console.log(answer);
     }
-  }, [amount, sides, modifier, change]);
+  }
 
   return (
     <DiceBox>
-      <RollDice
-        setAmount={setAmount}
-        setSides={setSides}
-        setModifier={setModifier}
-        amount={amount}
-        sides={sides}
-        modifier={modifier}
-      />
+      <Inputs>
+        <input
+          type="number"
+          id="amount"
+          min="1"
+          defaultValue={amount}
+          onBlur={(e) => amount = e.target.value}
+        />
+        d
+        <input
+          type="number"
+          id="sides"
+          min="2"
+          max="100"
+          defaultValue={sides}
+          onBlur={(e) => sides = e.target.value}
+        />
+        +
+        <input
+          type="number"
+          id="modifier"
+          min="0"
+          defaultValue={modifier}
+          onBlur={(e) => modifier = e.target.value}
+        />
 
-      <input
-        type="button"
-        value="Roll!"
-        onClick={change ? setChange(false) : setChange(true)}
-      />
+        <input
+          type="button"
+          value="Roll!"
+          onClick={() => {roll(amount, sides, modifier)}}
+        />
+      </Inputs>
 
       <AnswerBox answer={answer} />
     </DiceBox>
