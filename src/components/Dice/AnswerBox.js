ac93c823609439330461f79e77e1dc4f074d1afd
Savannaha <savwiley@yahoo.com>
2022-09-29 15:26:43 -0500

    add div around answers

--- src/components/Dice/AnswerBox.js
+++ src/components/Dice/AnswerBox.js
@@ -1,21 +1,25 @@
 import React from "react";
-import { Rolled } from "./style";
+import { DiceAnswers, Rolled } from "./style";
 import PropTypes from "prop-types";
 
 const AnswerBox = (props) => {
   const { answer } = props;
 
   return (
-    <>
+    <DiceAnswers>
       {answer.map((a, i) => (
         <Rolled key={i}>{a}</Rolled>
       ))}
-    </>
-  )
+    </DiceAnswers>
+  );
 };
 
 AnswerBox.propTypes = {
-  answer: PropTypes.PropTypes.oneOfType([PropTypes.object, PropTypes.string, PropTypes.array]),
+  answer: PropTypes.PropTypes.oneOfType([
+    PropTypes.object,
+    PropTypes.string,
+    PropTypes.array,
+  ]),
 };
 
 export default AnswerBox;
