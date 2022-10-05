04f174739229742ec27c195fed62b59f2c7d8ce5
Savannaha <savwiley@yahoo.com>
2022-10-05 17:17:43 -0500

    add color selector and dynamic grid

--- src/components/Grid/index.js
+++ src/components/Grid/index.js
@@ -1,7 +1,55 @@
-import React from "react";
+import React, { useState } from "react";
+import { GridBox, GridSpace, Square } from "./style";
 
 const Grid = () => {
-  return <></>;
+  const [color, setColor] = useState("#000000");
+  const [numb, setNumb] = useState(16);
+  const [numbSq, setNumbSq] = useState(16);
+
+
+  return (
+    <GridBox>
+
+      {/*change color*/}
+      <input 
+        type="color"
+        id="color"
+        value={color}
+        onChange={e => {
+          setColor(e.target.value);
+        }}
+      />
+
+      {/*change grid size*/}
+      <input
+        type="number"
+        id="number"
+        defaultValue={numb}
+        onBlur={(e) => {
+          setNumb(e.target.value);
+        }}
+      />
+      <input
+        type="button"
+        value="Resize"
+        onClick={() => {
+          setNumbSq(numb);
+          document.querySelector("#number").value = "";
+        }}
+      />
+
+      <GridSpace numbOfSquares={numbSq}>
+        {[...Array(numbSq * numbSq)].map((e, i) => (
+          <Square
+            className="square"
+            id={i}
+            key={i}
+          ></Square>
+        ))}
+      </GridSpace>
+
+    </GridBox>
+  );
 };
 
 export default Grid;
