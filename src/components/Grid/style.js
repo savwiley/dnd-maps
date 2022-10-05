83dfa961a474fed5088968f6804207d3815e3f37
Savannaha <savwiley@yahoo.com>
2022-10-05 17:17:26 -0500

    stylize inputs and grid with props

--- src/components/Grid/style.js
+++ src/components/Grid/style.js
@@ -1,1 +1,43 @@
 import styled from "styled-components";
+
+export const GridBox = styled.div`
+  margin: auto;
+  width: 60%;
+  text-align: center;
+  input {
+    margin: 5px 0;
+    padding: 2px;
+    background: var(--text);
+    border: 1px solid var(--background);
+    cursor: pointer;
+    color: var(--background);
+    font-weight: bold;
+    min-width: 50px;
+    width: 30px;
+  }
+  input[type="color"] {
+    display: block;
+    margin: 10px auto 0;
+  }
+  input:hover {
+    background: var(--background);
+    border-color: var(--text);
+    color: var(--text);
+  }
+`;
+
+export const GridSpace = styled.div`
+  display: grid;
+  grid-template-columns: repeat(${props => props.numbOfSquares}, 1fr);
+  grid-template-rows: repeat(${props => props.numbOfSquares}, 1fr);
+  width: 100%;
+  height: 65vh;
+  border: 1px solid var(--lightText);
+`;
+
+export const Square = styled.div`
+  border: 1px solid var(--lightAccent);
+  cursor: pointer;
+  width: 100%;
+  height: 100%;
+`;
