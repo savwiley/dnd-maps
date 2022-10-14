2555a622a02fa28272c53b45650f970ece686d2a
Savannaha <savwiley@yahoo.com>
2022-10-14 13:28:19 -0500

    place characters on either side of grid

--- src/components/Characters/style.js
+++ src/components/Characters/style.js
@@ -1,1 +1,15 @@
 import styled from "styled-components";
+
+export const Protags = styled.div`
+  display: inline-block;
+  float: left;
+  width: 25vw;
+  height: auto;
+`;
+
+export const Antags = styled.div`
+  display: inline-block;
+  float: right;
+  width: 25vw;
+  height: auto;
+`;
