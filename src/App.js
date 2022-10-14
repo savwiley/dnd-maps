ec71a927139d46653cd4539a99f9115c70608730
Savannaha <savwiley@yahoo.com>
2022-10-14 13:20:18 -0500

    import Characters

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import React from "react";
 import Grid from "./components/Grid/index.js";
 import Dice from "./components/Dice/index.js";
+import {Protagonists, Antagonists} from "./components/Characters/index.js";
 
 function App() {
   return (
     <div>
+      <Protagonists />
       <Grid />
+      <Antagonists />
       <Dice />
     </div>
   );
