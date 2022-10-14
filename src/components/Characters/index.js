196d0dbe613f84a4204781f20205a44d7417103f
Savannaha <savwiley@yahoo.com>
2022-10-14 13:20:40 -0500

    create Characters components

---
+++ src/components/Characters/index.js
@@ -0,0 +1,18 @@
+import React from "react";
+
+export const Protagonists = () => {
+
+  return (
+    <>
+    </>
+  )
+};
+
+
+export const Antagonists = () => {
+
+  return (
+    <>
+    </>
+  )
+};
