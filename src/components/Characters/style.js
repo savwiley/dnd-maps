a0a56c1269e8b497b67287864bcaf084c5670bde
Savannaha <savwiley@yahoo.com>
2022-11-09 21:23:10 -0600

    stylize the character modules

--- src/components/Characters/style.js
+++ src/components/Characters/style.js
@@ -26,3 +26,43 @@ export const Input = styled.div`
     color: var(--text);
   }
 `;
+
+export const CharModule = styled.div`
+  display: block;
+  width: 100%;
+  border-radius: 10px;
+  background: var(--lightAccent);
+  height: 70px;
+  margin: 10px 5px;
+  padding: 5px 10px;
+  font-weight: bold;
+  .name {
+    font-size: 20px;
+    font-weight: bold;
+    float: left;
+  }
+  .hp {
+    float: right;
+  }
+  .hpHolder {
+    display: block;
+    width: 100%;
+    border-radius: 5px;
+    height: 5px;
+    border: 1px solid var(--background);
+    background: var(--background);
+    margin: 30px 0 10px;
+  }
+  .hpActual {
+    width: ${(props) => props.HP}%;
+    height: 5px;
+    background: var(--lightText);
+    border-radius: 5px;
+  }
+  .armor {
+    float: left;
+  }
+  .initiative {
+    float: right;
+  }
+`;
