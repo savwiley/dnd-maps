5c29da148dbfa77bda65f3bf5382833387f4eb97
Savannaha <savwiley@yahoo.com>
2022-11-10 00:26:03 -0600

    add edit option to character modules

--- src/components/Characters/CharModules.js
+++ src/components/Characters/CharModules.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { CharModule } from "./style";
 
 const CharModules = (props) => {
@@ -6,15 +6,37 @@ const CharModules = (props) => {
   const [newHP, setNewHP] = useState(100);
 
   //finds percent of new HP
-  const changeHP = (hit) => {
-    const total = curHp - hit;
-    const decimal = total / hp;
+  useEffect(() => {
+    const decimal = curHp / hp;
     const percent = decimal * 100;
     setNewHP(percent);
-  };
+  },[curHp, hp]);
 
   return (
-    <CharModule HP={newHP}>
+    <CharModule HP={newHP}
+      onClick={() => {
+        const cName = document.getElementById("nameInput");
+        cName.value = name;
+        cName.focus();
+        cName.blur();
+        const cCurHP = document.getElementById("curHPInput");
+        cCurHP.value = curHp;
+        cCurHP.focus();
+        cCurHP.blur();
+        const cHP = document.getElementById("maxHPInput");
+        cHP.value = hp;
+        cHP.focus();
+        cHP.blur();
+        const cArmor = document.getElementById("armorInput");
+        cArmor.value = armor;
+        cArmor.focus();
+        cArmor.blur();
+        const cInit = document.getElementById("initInput");
+        cInit.value = init;
+        cInit.focus();
+        cInit.blur();
+      }}
+    >
       {/*name*/}
       <div className="name">{name}</div>
       {/*HP*/}
@@ -34,3 +56,16 @@ const CharModules = (props) => {
 };
 
 export default CharModules;
+
+
+
+
+
+
+/**
+ * - make sure all inputs are filled
+ * - make sure current HP doesn't exceed max HP
+ * - make sure init is never less than 1
+ * - fix spacing for inputs/button
+ * - add delete
+ */
