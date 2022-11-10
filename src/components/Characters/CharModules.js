9e038ccfe2fd6c8c3b8613e27c6cb475e7f75a48
Savannaha <savwiley@yahoo.com>
2022-11-09 21:23:50 -0600

    create character modules every time char is added

--- src/components/Characters/CharModules.js
+++ src/components/Characters/CharModules.js
@@ -1,9 +1,34 @@
-import React from "react";
+import React, {useState} from "react";
+import { CharModule } from "./style";
 
 const CharModules = (props) => {
-  const [name, hp, curHp, armor, init] = props;
+  const { name, hp, curHp, armor, init } = props;
+  const [newHP, setNewHP] = useState(100);
 
-  return <></>;
+  //finds percent of new HP
+  const changeHP = (hit) => {
+    const total = curHp - hit;
+    const decimal = total / hp;
+    const percent = decimal * 100;
+    setNewHP(percent);
+  }
+
+  return (
+    <CharModule HP={newHP}>
+      {/*name*/}
+      <div className="name">{name}</div>
+      {/*HP*/}
+      <div className="hp">{curHp} / {hp}</div>
+      {/*HP scale*/}
+      <div className="hpHolder">
+        <div className="hpActual"></div>
+      </div>
+      {/*armor*/}
+      <div className="armor">AC {armor}</div>
+      {/*initiative*/}
+      <div className="initiative">INIT {init}</div>
+    </CharModule>
+  );
 };
 
 export default CharModules;
