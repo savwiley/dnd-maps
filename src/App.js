b9a01177cf8022f9cb4b016fcfa2f050da6dc777
Savannaha <savwiley@yahoo.com>
2022-11-11 14:42:44 -0600

    add github link

--- src/App.js
+++ src/App.js
@@ -2,6 +2,8 @@ import React from "react";
 import Grid from "./components/Grid/index.js";
 import Dice from "./components/Dice/index.js";
 import Characters from "./components/Characters/index.js";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faSquareGithub } from "@fortawesome/free-brands-svg-icons";
 
 function App() {
   return (
@@ -9,6 +11,9 @@ function App() {
       <Characters />
       <Grid />
       <Dice />
+      <a href="https://github.com/savwiley/dnd-maps" className="github">
+        <FontAwesomeIcon icon={faSquareGithub} />
+      </a>
     </div>
   );
 }
