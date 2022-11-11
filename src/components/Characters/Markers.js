2cc4554c43f0fef6fdbf42c00b329419e4a6c07e
Savannaha <savwiley@yahoo.com>
2022-11-10 21:22:14 -0600

    create component for character markers

---
+++ src/components/Characters/Markers.js
@@ -0,0 +1,21 @@
+import React from "react";
+
+const Marker = (props) => {
+  const { name } = props;
+
+  return(
+    <>
+    </>
+  )
+};
+
+export default Marker;
+
+
+
+
+
+
+/**
+ * - https://www.w3schools.com/howto/howto_js_draggable.asp
+ */
